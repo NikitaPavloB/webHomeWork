2d2c15ecc7a670b20f21a688fad9028d0c39a5cd
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2024-04-05 16:28:30 +0300

    Update test_conftest.py

--- test_conftest.py
+++ test_conftest.py
@@ -16,3 +16,15 @@ def test1_check_post(auth_token, get_title_post):
     print(res)
     assert get_title_post in res, f"Пост с заголовком '{get_title_post}' не найден среди полученных данных: {res}"
 
+def test2_create_post(auth_token):
+    url = data['post_url']
+    headers = {'X-Auth-Token': auth_token}
+    new_post_data = {
+        'title': data['post_title'],
+        'description': data['post_description'],
+        'content': data['post_content']
+    }
+    response = S.post(url=url, headers=headers, json=new_post_data)
+
+    assert response.status_code == 200, f"Failed to create post. Status code: {response.status_code}"
+
