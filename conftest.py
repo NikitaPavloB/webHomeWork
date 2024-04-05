02a826a5ec0a3552f4b25ae1e95b777d3a55ed45
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2024-04-05 13:41:16 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -7,6 +7,7 @@ with open("config.yaml", encoding='utf-8') as f:
     data = yaml.safe_load(f)
 S = requests.Session()
 
+
 @pytest.fixture()
 def auth_token():
     response = S.post(url=data['login_url'], data={"username": data['username'], "password": data['password']})
@@ -16,3 +17,7 @@ def auth_token():
 
     if response.status_code == 200:
         return response.json()['token']
+
+@pytest.fixture()
+def get_title_post():
+    return 'Kek'
