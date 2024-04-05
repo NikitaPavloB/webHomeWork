d6919b0a4626ae46a12469a3b17a5c5b727ec734
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2024-04-05 13:40:54 +0300

    Update test_conftest.py

--- test_conftest.py
+++ test_conftest.py
@@ -1,5 +1,18 @@
 import pytest
-from conftest import auth_token
+import requests
+import yaml
 
-def test_auth_token(auth_token):
-    assert auth_token is not None, "Failed to get auth token"
+S = requests.Session()
+
+with open("config.yaml", encoding='utf-8') as f:
+    data = yaml.safe_load(f)
+
+def test1_check_post(auth_token, get_title_post):
+    url = data['post_url']
+    headers = {'X-Auth-Token': auth_token}
+    not_my_post = {"owner": "notMe"}
+    data_json = S.get(url=url, params=not_my_post, headers=headers).json()['data']
+    res = [x['title'] for x in data_json]
+    print(res)
+    assert get_title_post in res, f"Пост с заголовком '{get_title_post}' не найден среди полученных данных: {res}"
+
