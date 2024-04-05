5e42dd304d1e7ebdf0c3b93c1d8394b24205b89b
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2024-04-05 13:18:04 +0300

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,18 @@
+import json
+import pytest
+import requests
+import yaml
+
+with open("config.yaml", encoding='utf-8') as f:
+    data = yaml.safe_load(f)
+S = requests.Session()
+
+@pytest.fixture()
+def auth_token():
+    response = S.post(url=data['login_url'], data={"username": data['username'], "password": data['password']})
+
+    formatted_response = json.dumps(response.json(), indent=4, ensure_ascii=False)
+    print("Response from server:", formatted_response)
+
+    if response.status_code == 200:
+        return response.json()['token']
