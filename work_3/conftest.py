0bfaf2b4eabf013ad25bf773dd0085dc52c0c820
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2024-04-13 01:35:24 +0300

    Create conftest.py

---
+++ work_3/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+import yaml
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+from module import Site
+
+with open('datatest.yaml', 'r', encoding='utf-8') as f:
+    data = yaml.safe_load(f)
+    browser_name = data['browser']
+
+
+@pytest.fixture(scope='session')
+def browser():
+    if browser_name == 'firefox':
+        service = Service(executable_path=GeckoDriverManager().install())
+        options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox(service=service, options=options)
+
+    else:
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service=service, options=options)
+    yield driver
+    driver.quit()
+
