75ce8cd23fd87cf56968c231421028f5462239db
Nikita_Pavlov <126702146+NikitaPavloB@users.noreply.github.com>
2024-04-16 11:40:20 +0300

    Update conftest.py

--- work_4/conftest.py
+++ work_4/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -10,6 +11,8 @@ with open('datatest.yaml', 'r', encoding='utf-8') as f:
     data = yaml.safe_load(f)
     browser_name = data['browser']
 
+# Фикстуры для тестов UI
+
 
 @pytest.fixture(scope='session')
 def browser():
@@ -17,14 +20,12 @@ def browser():
         service = Service(executable_path=GeckoDriverManager().install())
         options = webdriver.FirefoxOptions()
         driver = webdriver.Firefox(service=service, options=options)
-
     else:
         service = Service(executable_path=ChromeDriverManager().install())
         options = webdriver.ChromeOptions()
         driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
-
 
 @pytest.fixture(scope='session', autouse=True)
 def send_email_after_tests():
@@ -37,7 +38,26 @@ def send_email_after_tests():
     )
     # выполнение отправки email после всех тестов
     yield
-
     # выполнение отправки email
     email_sender.send_email()
 
+# Фикстуры для тестов API
+
+
+@pytest.fixture()
+def auth_token():
+    try:
+        response = requests.post(
+            url=data['login_url'],
+            data={"username": data['login'], "password": data['password']}
+        )
+        response.raise_for_status()
+        if response.status_code == 200:
+            return response.json()['token']
+    except requests.RequestException as e:
+        pytest.fail(f"Failed to authenticate: {e}")
+
+
+@pytest.fixture()
+def get_title_post():
+    return 'Test'
